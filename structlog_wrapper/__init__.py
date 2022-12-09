7b11b9fa8091f6255ccf0cd468af3a991f030353
Manish Garg <36438090+manish659@users.noreply.github.com>
2022-12-09 17:05:09 +0530

    Update __init__.py

--- structlog_wrapper/__init__.py
+++ structlog_wrapper/__init__.py
@@ -1,6 +1,6 @@
 default_app_config = "structlog_wrapper.apps.StructLogWrapperConfig"
 name = "structlog_wrapper"
 
-VERSION = (1, 0, 2)
+VERSION = (1, 1, 0)
 
 __version__ = ".".join(str(v) for v in VERSION)
